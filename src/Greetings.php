70cb9c4a408e740a245604bda846db705dd4c515
sajjad-xs <sajjad@xpeedstudio.com>
2022-11-23 14:30:31 +0600

    add str slice

--- src/Greetings.php
+++ src/Greetings.php
@@ -9,4 +9,15 @@ class Greetings
     {
         return "Hello! $greeting";
     }
+
+    public function strcut(String $string, $beginning = false, $ending = false)
+    {
+        $beginningPos = strpos($string, "$beginning");
+        $tmpstring = substr($string, $beginningPos);
+        $endPos = strpos($tmpstring, "$ending");
+        if ($beginningPos === false || $endPos === false) {
+            return $string;
+        }
+        return substr($string, $beginningPos, ($endPos + strlen("$ending")));
+    }
 }
