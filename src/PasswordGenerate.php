30280067dbe6f41ce7bb1f510e592a1c3c0e2c1d
sajjad-xs <sajjad@xpeedstudio.com>
2022-11-24 11:11:34 +0600

    updtaed

--- src/PasswordGenerate.php
+++ src/PasswordGenerate.php
@@ -14,4 +14,14 @@ class PasswordGenerate
         $random = substr(strtoupper(md5(mt_rand())), 0, $length);
         return $random;
     }
+    public function RandomString($length)
+    {
+        $keys = array_merge(range(0, 9), range('a', 'z'));
+
+        $key = "";
+        for ($i = 0; $i < $length; $i++) {
+            $key .= $keys[mt_rand(0, count($keys) - 1)];
+        }
+        return $key;
+    }
 }
